8c166ffd820c799cfa8be82511b65bed14fde7d5
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2023-12-26 02:45:36 +0100

    Create etree.hpp
    - Added an hpp to make it easier to read and maintain the code.

---
+++ include/etree.hpp
@@ -0,0 +1,17 @@
+#ifndef ETREE_HPP
+#define ETREE_HPP
+
+#include <string>
+
+namespace Colours {
+    const std::string greenColour = "\033[1;32m";
+    const std::string endColour = "\033[0m";
+    const std::string redColour = "\033[1;31m";
+    const std::string blueColour = "\033[1;34m";
+    const std::string yellowColour = "\033[1;33m";
+    const std::string purpleColour = "\033[1;35m";
+    const std::string turquoiseColour = "\033[1;36m";
+    const std::string grayColour = "\033[1;37m";
+}
+
+#endif
