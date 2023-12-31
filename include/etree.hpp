e85047c258c0d15c937582479580a37f5c48bf26
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2023-12-31 04:02:28 +0100

    Update etree.hpp

--- include/etree.hpp
+++ include/etree.hpp
@@ -2,8 +2,23 @@
 #define ETREE_HPP
 
 #include <map>
+#include <set>
 #include <string>
+#include <fstream>
 #include <cstdlib>
+#include <iostream>
+#include "json.hpp"
+#include <filesystem>
+
+using json = nlohmann::json;
+using namespace std;
+namespace fs = filesystem;
+
+size_t dirs = 0;
+size_t files = 0;
+std::set<std::string> visited_links;
+vector<string> inner_pointers = { "├── ", "│   " };
+vector<string> final_pointers = { "└── ", "    " };
 
 namespace Colours {
     const std::string greenColour = "\033[1;32m";
@@ -30,5 +45,77 @@ namespace Paths {
         return getEtreeFolderPath() + "locales/" + languageCode + ".json";
     }
 }
+
+//Curl function
+inline size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *buffer) {
+    buffer->append(static_cast<char *>(contents), size * nmemb);
+    return size * nmemb;
+}
+
+//Translation Upload
+std::map<std::string, std::string> loadTranslations(const std::string& filePath) {
+    std::map<std::string, std::string> translations;
+
+    std::ifstream file(filePath);
+    if (file.is_open()) {
+        json data;
+        file >> data;
+
+        for (json::iterator it = data.begin(); it != data.end(); ++it) {
+            translations[it.value()] = it.key();
+        }
+
+        file.close();
+    } else {
+        std::cerr << "Error in the translation file" << std::endl;
+    }
+
+    return translations;
+}
+
+//Icons + Extensions
+const std::vector<std::pair<std::string, std::string>> iconsWithExtensions = {
+    {" ", ""},
+    {" ", ".cpp"},
+    {" ", ".py"},
+    {" ", ""},
+    {" ", ".db"},
+    {" ", ""},
+    {" ", ".js"},
+    {" ", ".zip"},
+    {" ", ".jar"},
+    {" ", ".php"},
+    {" ", ".txt"},
+    {"󰌛 ", ".cs"},
+    {" ", ".ts"},
+    {" ", ".c"},
+    {" ", ".rb"},
+    {" ", ".css"},
+    {" ", ".html"},
+    {" ", ".json"},
+    {" ", ".hpp"},
+    {" ", ".sh"},
+    {" ", ".rs"},
+    {" ", ".mp4"},
+    {" ", ".mov"},
+    {" ", ".avi"},
+    {" ", ".mkv"},
+    {" ", ".flv"},
+    {" ", ".wmv"},
+    {" ", ".divx"},
+    {" ", ".xvid"},
+    {" ", ".png"},
+    {" ", ".jpg"},
+    {" ", ".jpeg"},
+    {"󰵸 ", ".gif"},
+    {" ", ".webp"},
+    {" ", ".tiff"},
+    {" ", ".bmp"},
+    {" ", ".heif"},
+    {" ", ".svg"},
+    {" ", ".eps"},
+    {" ", ".psd"},
+    {"󰨊 ", ".ps1"}
+};
 
 #endif
