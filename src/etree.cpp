95e6ab7db8afbbbaadf264290c1319681ab54a5a
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2024-04-06 04:20:06 +0200

    Update etree.cpp
    - Removed the code to update the tool from etree.cpp, to make it more modular.

--- src/etree.cpp
+++ src/etree.cpp
@@ -1,6 +1,6 @@
 #include "../include/etree.hpp"
 #include "../include/json.hpp"
-#include <curl/curl.h>
+#include "../include/update.hpp"
 #include <algorithm>
 #include <vector>
 
@@ -310,41 +310,6 @@ void printDirectory(const std::string& directory, bool parameNFlag) {
         }
     }
     std::cout << output << std::flush;
-}
-
-void checkupdate(const std::map<std::string, std::string>& translations) {
-    CURL *curl;
-    CURLcode res;
-    std::string buffer;
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    std::string version_translations = translations.at("version");
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-
-        res = curl_easy_perform(curl);
-
-        if (res != CURLE_OK) {
-            std::cerr << translations.at("error_version_download") << curl_easy_strerror(res) << std::endl;
-        } else {
-            size_t pos = buffer.find("v.");
-            if (pos != std::string::npos) {
-                std::string download_ver = buffer.substr(pos, 7);
-                if (version_translations == download_ver) {
-                    std::cout << translations.at("version_is_update") << std::endl;
-                } else {
-                    std::cout << translations.at("version_need_update") << std::endl;
-                }
-            } else {
-                std::cout << translations.at("no_version_found") << std::endl;
-            }
-        }
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
 }
 
 void showHelp(const std::map<std::string, std::string>& translations) {
