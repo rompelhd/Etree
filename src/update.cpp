e5e5bf473381bc4cbfb1a63b536afcd64ff6477d
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2024-05-07 17:48:04 +0200

    Update update.cpp
    - Improved the appearance of the updater views, now it looks cleaner, organized, and is responsive.

--- src/update.cpp
+++ src/update.cpp
@@ -9,7 +9,97 @@ std::string url = "https://raw.githubusercontent.com/rompelhd/Etree/main/version
 std::string tags_url = "https://github.com/rompelhd/Etree/tags";
 std::string ver_url = "https://github.com/rompelhd/Etree/releases/tag/";
 
-void checkupdate(const std::map<std::string, std::string>& translations) {
+int getTerminalWidth() {
+    winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    return w.ws_col;
+}
+
+void hightable(int totalWidth, const std::string& version) {
+    std::cout << Colours::blueColour << "╭─ Changes in [ " << Colours::yellowColour << version << Colours::blueColour << " ] ";
+    for (int i = 26; i < totalWidth - 1; ++i) {
+        std::cout << "─";
+    }
+    std::cout << "╮" << std::endl;
+    std::cout << Colours::blueColour << "│";
+    for (int i = 1; i < totalWidth - 1; ++i) {
+        std::cout << " ";
+    }
+    std::cout << Colours::blueColour << "│" << std::endl;
+}
+
+size_t getRealLength(const std::string& str) {
+    size_t length = 0;
+    bool inEscape = false;
+
+    for (char c : str) {
+        if (c == '\033') {
+            inEscape = true;
+        } else if (inEscape && c == 'm') {
+            inEscape = false;
+        } else if (!inEscape) {
+            length++;
+        }
+    }
+
+    return length;
+}
+
+void intertable(int totalWidth, const std::string& li_content, int terminalWidth) {
+    std::string output = "\033[34m│  [*] \033[33m" + li_content + "\033[0m";
+    size_t realLength = getRealLength(output);
+    size_t spacesNeeded = totalWidth - realLength;
+
+    std::string additionalSpacesFirst;
+    std::string additionalSpacesSecond;
+
+      if (realLength >= terminalWidth) {
+        size_t lastSpace = output.rfind(' ', terminalWidth);
+
+        std::string firstLine = output.substr(0, lastSpace);
+        std::string secondLine = output.substr(lastSpace + 1);
+
+        size_t firstLineLength = getRealLength(firstLine);
+        size_t secondLineLength = getRealLength(secondLine);
+
+        size_t remainingSpacesFirst = totalWidth - firstLineLength + 1;
+        for (size_t i = 0; i < remainingSpacesFirst; ++i) {
+            additionalSpacesFirst += " ";
+        }
+
+        size_t remainingSpacesSecond = totalWidth - secondLineLength - 8;
+        for (size_t i = 0; i < remainingSpacesSecond; ++i) {
+            additionalSpacesSecond += " ";
+        }
+
+        std::cout << firstLine << additionalSpacesFirst << Colours::blueColour << "│" << std::endl;
+        std::cout << Colours::blueColour << "│      " << Colours::yellowColour << secondLine << additionalSpacesSecond << Colours::blueColour << "│" << std::endl;
+
+
+    } else {
+        std::cout << Colours::blueColour << output;
+        for (int i = 0; i < spacesNeeded; ++i) {
+            std::cout << " ";
+        }
+        std::cout << Colours::blueColour << " │";
+    }
+}
+
+void lowtable(int totalWidth) {
+    std::cout << Colours::blueColour << "│";
+    for (int i = 1; i < totalWidth - 1; ++i) {
+        std::cout << " ";
+    }
+    std::cout << "│" << std::endl;
+
+    std::cout << Colours::blueColour << "╰";
+    for (int i = 1; i < totalWidth - 1; ++i) {
+        std::cout << "─";
+    }
+    std::cout << "╯" << std::endl;
+}
+
+int checkupdate(const std::map<std::string, std::string>& translations) {
     CURL *curl;
     CURLcode res;
     std::string buffer;
@@ -44,6 +134,12 @@ void checkupdate(const std::map<std::string, std::string>& translations) {
                         curl_easy_setopt(curl_tags, CURLOPT_WRITEDATA, &buffer);
                         res = curl_easy_perform(curl_tags);
                         if (res == CURLE_OK) {
+                            int terminalWidth = getTerminalWidth();
+                            if (terminalWidth < 85) {
+                                std::cout << "Error: Tamaño de terminal no válido." << std::endl;
+                                return 1;
+                            }
+
                             std::set<std::string> unique_versions;
                             std::regex version_regex("v\\.[0-9]+\\.[0-9]+\\.[0-9]+");
                             std::smatch match;
@@ -64,7 +160,7 @@ void checkupdate(const std::map<std::string, std::string>& translations) {
                                 for (size_t i = 0; i < outdated_versions.size() - 1; ++i) {
                                     std::cout << Colours::yellowColour << outdated_versions[i] << Colours::blueColour << ", ";
                                 }
-                                std::cout << Colours::yellowColour << outdated_versions.back() << Colours::blueColour << "]" << Colours::endColour << std::endl;
+                                std::cout << Colours::yellowColour << outdated_versions.back() << Colours::blueColour << "]" << "\n" << Colours::endColour << std::endl;
 
                                 for (const auto& version : outdated_versions) {
                                     std::string tag_url = ver_url + version;
@@ -78,19 +174,21 @@ void checkupdate(const std::map<std::string, std::string>& translations) {
                                         res = curl_easy_perform(curl_tag);
                                         if (res == CURLE_OK) {
 
-                                            std::regex commits_regex("<a href=\".*\">\\s*(\\d+) commits\\s*</a>");
-                                            if (std::regex_search(buffer_tag, match, commits_regex)) {
-                                                std::cout << Colours::blueColour << "\nChanges in [ " << Colours::yellowColour << version << Colours::blueColour << " ] " << "Commits until the last version: " << Colours::yellowColour << match[1] << "\n" << Colours::endColour << std::endl;
-                                            }
+                                            hightable(terminalWidth, version);
 
                                             std::regex li_regex("<li>(.+?)<\\/li>");
                                             std::smatch match;
                                             std::string::const_iterator search_start(buffer_tag.cbegin());
+                                            std::string li_content;
                                             while (std::regex_search(search_start, buffer_tag.cend(), match, li_regex)) {
                                                 std::string li_content = match[1];
-                                                std::cout << Colours::blueColour << "[*] " << Colours::yellowColour << li_content << Colours::endColour << std::endl;
+
+                                                intertable(terminalWidth, li_content, terminalWidth);
+
                                                 search_start = match.suffix().first;
                                             }
+
+                                            lowtable(terminalWidth);
 
                                         } else {
                                             std::cerr << translations.at("error_tag_download") << curl_easy_strerror(res) << std::endl;
@@ -114,4 +212,5 @@ void checkupdate(const std::map<std::string, std::string>& translations) {
         curl_easy_cleanup(curl);
     }
     curl_global_cleanup();
+    return 0;
 }
